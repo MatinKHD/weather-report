fe1170299144b69e475fce756fe01b759daab1f5
Matin KHD <matink714@gmail.com>
2023-10-16 12:28:01 +0330

    craete footer component

---
+++ src/footer.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  <nav aria-label="navigation for pages">
+    <ul>
+      <li>
+        <Link to="/Home"></Link>
+      </li>
+      <li>
+        <Link to="/About"></Link>
+      </li>
+    </ul>
+  </nav>;
+}
+
+export default Footer;
