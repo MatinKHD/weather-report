8ef764a5e32ddcf627050b37af8ad7a498c070bd
Matin KHD <matink714@gmail.com>
2023-10-16 12:30:53 +0330

    fix some bugs in footer.jsx

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 
 function Footer() {
-  <nav aria-label="navigation for pages">
-    <ul>
-      <li>
-        <Link to="/Home"></Link>
-      </li>
-      <li>
-        <Link to="/About"></Link>
-      </li>
-    </ul>
-  </nav>;
+  return (
+    <nav aria-label="navigation for pages">
+      <ul>
+        <li>
+          <Link to="/Home">Home</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Footer;
