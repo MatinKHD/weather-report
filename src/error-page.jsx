4101ad6b73552c9cc8d8f8083b71d0e9ede3f6b4
Matin KHD <matink714@gmail.com>
2023-10-16 12:21:24 +0330

    create error-page component

---
+++ src/error-page.jsx
@@ -0,0 +1,19 @@
+import { useRouteError } from "react-router";
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div className="error-page">
+      <h1> Opps !</h1>
+      <p> sorry an unexpected error has occured</p>
+      <p>
+        <i>{error.statusText}</i>
+        <i>{error.massage}</i>
+      </p>
+    </div>
+  );
+}
+
+export default ErrorPage;
