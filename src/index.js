382039c0f8512009fa990ba1e0085afad9b9ec40
Matin KHD <matink714@gmail.com>
2023-10-16 12:15:46 +0330

    add App component and configure routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { reportWebVitals } from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -8,7 +8,12 @@ import "./index.css";
 
 import App from "./app";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
