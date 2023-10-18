19f92d9384756115f1f45b49b0100cdf8550cedc
Matin KHD <matink714@gmail.com>
2023-10-18 14:39:21 +0330

    fix some bugs in loader function and get the datain report component

--- src/report.jsx
+++ src/report.jsx
@@ -1,11 +1,17 @@
+import { useLoaderData } from "react-router";
 import { weatherData } from "./weatherData";
+
 export async function loader({ params }) {
-  const [scale] = params;
+  const { scale } = params;
   const data = await weatherData(scale).data;
 
   return { data, scale };
 }
 
-function Report() {}
+function Report() {
+  const data = useLoaderData();
+
+  console.log(data);
+}
 
 export default Report;
